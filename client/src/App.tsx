371b03a27ad771cc2415cb6ed538916ffbda223d
Callum <callummcluskey100@gmail.com>
2022-09-04 16:59:54 +0100

    added basic showing off sent image and eraser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ReactSketchCanvas } from 'react-sketch-canvas';
 import styled from 'styled-components'
 
-const Canvas = class extends React.Component<any> {
+const Canvas = class extends React.Component<any,any> {
   canvas: React.RefObject<any>;
+  saveImage: Function;
   constructor(props:any) {
     super(props);
-
+    this.saveImage = props.saveImage;
     this.canvas = React.createRef();
+    this.state = {eraseMode:false};
   }
+
+  changeHandler() {
+    this.setState({ eraseMode: !this.state.eraseMode }, () => 
+    console.log(this.state.eraseMode ));
+ }
+
 
   render() {
     return (
       <>
         <ReactSketchCanvas
         style={{
-          border: `3px solid ${this.props.color}`,
+          border: `3px solid gray`,
           borderRadius: '8px',
           backgroundSize:'contain'
         }}
@@ -31,7 +39,8 @@ const Canvas = class extends React.Component<any> {
               .exportImage("png")
               .then((data:any) => {
                 // here is where the image will be available
-                console.log(data);
+                this.saveImage(data);
+                this.canvas.current.clearCanvas()
               })
           }}
         >
@@ -48,6 +57,17 @@ const Canvas = class extends React.Component<any> {
         >
           Clear
         </button>
+        <button
+        style={{top:"400px"}}
+          onClick={() => {
+            this.canvas.current
+              .eraseMode(!this.state.eraseMode)
+              this.changeHandler()
+             
+          }}
+        >
+          Eraser
+        </button>
       </>
     );
   }
@@ -57,8 +77,33 @@ const Canvas = class extends React.Component<any> {
 function App() {
 
   const [textRange, setTextRange] = React.useState("");
+  const [sentText,setSentText]  = React.useState("");
+  const [image,setImage] = React.useState("");
+
+  const saveImage = (data:string) => {
+    setImage(data);
+    console.log(textRange)
+  }
+
+  useEffect(()=>{
+    setSentText(textRange)
+    setTextRange("")
+  },[image])
 
   return (
+    <>
+        <div style={{position:"relative", width:"100%",display:"flex",justifyContent: "center",alignItems: "center",flexDirection:"column"}}>
+
+    <CanvasContainer style={{backgroundImage: `url(${image})`,backgroundSize:"cover", border: `3px solid gray`,
+          borderRadius: '8px'}}>
+    <TextContainer color="red" style={{left:-3, top: -3}}>
+      <div><h2>callum</h2></div>
+          <p>{sentText}</p> 
+        </TextContainer>
+    </CanvasContainer>
+    </div>
+
+    <br/><br/><br/><br/><br/><br/>
 
     <div style={{position:"relative", width:"100%",display:"flex",justifyContent: "center",alignItems: "center",flexDirection:"column"}}>
       <CanvasContainer>
@@ -66,7 +111,7 @@ function App() {
       <div><h2>callum</h2></div>
           <p>{textRange}</p> 
         </TextContainer>
-        <Canvas color="red"/>
+        <Canvas saveImage={saveImage}/>
         <TextContainer>
           <div><h2>callum</h2></div>
           <p></p> 
@@ -79,7 +124,7 @@ function App() {
       <input onChange={(e:any)=>setTextRange(e.target.value)}/>
       
     </div>
-
+    </>
   );
 }
 
@@ -96,8 +141,7 @@ word-break: break-word;
 
   }
   & div{
-    border: 3px solid;
-    border-color: ${props => props.color};
+    border: 3px solid gray;
     display: inline-block;
     width: 150px;
     height:40px;
