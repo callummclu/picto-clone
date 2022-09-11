e97b9a467c3e38c4993bd2b23d1ca02de39b42bb
Callum <callummcluskey100@gmail.com>
2022-09-11 13:39:04 +0100

    remade homepage

--- client/src/icons/CanvasIcons.tsx
+++ client/src/icons/CanvasIcons.tsx
@@ -1,65 +1,208 @@
 export const BigBrushIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="none" viewBox="0 0 11 11">
-  <circle cx="5.5" cy="5.5" r="5.5" fill="#5C5D5A"/>
-  <circle cx="5.5" cy="5.5" r="5.5" fill="#5C5D5A"/>
-</svg>
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="11"
+    height="11"
+    fill="none"
+    viewBox="0 0 11 11"
+  >
+    <circle cx="5.5" cy="5.5" r="5.5" fill="#5C5D5A" />
+    <circle cx="5.5" cy="5.5" r="5.5" fill="#5C5D5A" />
+  </svg>
+);
 
 export const SmallBrushIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="9" height="9" fill="none" viewBox="0 0 9 9">
-  <circle cx="4.5" cy="4.5" r="4.5" fill="#5C5D5A"/>
-</svg>
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="9"
+    height="9"
+    fill="none"
+    viewBox="0 0 9 9"
+  >
+    <circle cx="4.5" cy="4.5" r="4.5" fill="#5C5D5A" />
+  </svg>
+);
 
 export const PenToolIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 25 25">
-  <path stroke="#5A5A58" stroke-width="3" d="M5.667 22.516 22.605 6.088a.102.102 0 0 0 .027-.048c1.073-4.208-2.27-4.323-4.105-3.849a.097.097 0 0 0-.047.028L2.027 19.585a.1.1 0 0 0-.027.069v3.232a.1.1 0 0 0 .113.1l3.554-.47Zm0 0c4.682.38 6.21-.764 6.407-1.416a.105.105 0 0 1 .038-.056c1.538-1.129 4.649-2.77 5.013-.463.458 2.903 5.958-2.902 4.583-3.87-1.1-.774 1.07-1.612 2.292-1.935"/>
-</svg>
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="25"
+    height="25"
+    fill="none"
+    viewBox="0 0 25 25"
+  >
+    <path
+      stroke="#5A5A58"
+      stroke-width="3"
+      d="M5.667 22.516 22.605 6.088a.102.102 0 0 0 .027-.048c1.073-4.208-2.27-4.323-4.105-3.849a.097.097 0 0 0-.047.028L2.027 19.585a.1.1 0 0 0-.027.069v3.232a.1.1 0 0 0 .113.1l3.554-.47Zm0 0c4.682.38 6.21-.764 6.407-1.416a.105.105 0 0 1 .038-.056c1.538-1.129 4.649-2.77 5.013-.463.458 2.903 5.958-2.902 4.583-3.87-1.1-.774 1.07-1.612 2.292-1.935"
+    />
+  </svg>
+);
 
 export const EraserToolIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="25" fill="none" viewBox="0 0 28 25">
-  <path stroke="#5A5A58" stroke-width="3" d="m2 8.765 11.189-6.637a4 4 0 0 1 3.869-.117L26 6.608M2 8.765l13.09 6.47M2 8.765V14.4a4 4 0 0 0 2.324 3.632L15.091 23M26 6.608l-10.91 8.627M26 6.608v5.08a4 4 0 0 1-1.375 3.018L15.091 23m0-7.765V23"/>
-</svg>
-
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="28"
+    height="25"
+    fill="none"
+    viewBox="0 0 28 25"
+  >
+    <path
+      stroke="#5A5A58"
+      stroke-width="3"
+      d="m2 8.765 11.189-6.637a4 4 0 0 1 3.869-.117L26 6.608M2 8.765l13.09 6.47M2 8.765V14.4a4 4 0 0 0 2.324 3.632L15.091 23M26 6.608l-10.91 8.627M26 6.608v5.08a4 4 0 0 1-1.375 3.018L15.091 23m0-7.765V23"
+    />
+  </svg>
+);
 
 export const SendIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="58" height="66" fill="none" viewBox="0 0 58 66">
-  <path stroke="#84807F" stroke-width="3" d="M45 53.5c12.5-2 16.5 11.5 0 10.5m3-10.5V64M26 53h-8.9a.1.1 0 0 0-.1.1v5.138M26 64h-8.9a.1.1 0 0 1-.1-.1v-5.662m9 0h-9M30 64.5V53.234a.1.1 0 0 1 .169-.073L40.831 63.34a.1.1 0 0 0 .169-.073V52m-28.5 1.5c-3.485-.664-9.548-.997-8.02 4.923a.103.103 0 0 0 .097.077h7.359c.039 0 .074.022.09.058.952 2.188-.303 6.631-9.026 5.442"/>
-  <path fill="#fff" stroke="#84807F" stroke-width="2.25" d="M1.875 29.875h54.25v16.25H1.875z"/>
-  <path fill="#fff" stroke="#84807F" stroke-width="2.25" d="M1.875 29.875h13.25v5.25H1.875z"/>
-  <path fill="#84807F" d="M26 34h11v3H26zm0 5h11v3H26z"/>
-  <path fill="#84807F" stroke="#fff" stroke-width="2" d="M25 10h13v23H25z"/>
-  <path fill="#84807F" stroke="#fff" stroke-width="2" d="m32.22 2.307-.72-.75-.72.75-15.156 15.75-1.63 1.693h35.011l-1.629-1.693-15.155-15.75Z"/>
-  <path fill="#84807F" d="M26 16h11v9H26z"/>
-</svg>
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="58"
+    height="66"
+    fill="none"
+    viewBox="0 0 58 66"
+  >
+    <path
+      stroke="#84807F"
+      stroke-width="3"
+      d="M45 53.5c12.5-2 16.5 11.5 0 10.5m3-10.5V64M26 53h-8.9a.1.1 0 0 0-.1.1v5.138M26 64h-8.9a.1.1 0 0 1-.1-.1v-5.662m9 0h-9M30 64.5V53.234a.1.1 0 0 1 .169-.073L40.831 63.34a.1.1 0 0 0 .169-.073V52m-28.5 1.5c-3.485-.664-9.548-.997-8.02 4.923a.103.103 0 0 0 .097.077h7.359c.039 0 .074.022.09.058.952 2.188-.303 6.631-9.026 5.442"
+    />
+    <path
+      fill="#fff"
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 29.875h54.25v16.25H1.875z"
+    />
+    <path
+      fill="#fff"
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 29.875h13.25v5.25H1.875z"
+    />
+    <path fill="#84807F" d="M26 34h11v3H26zm0 5h11v3H26z" />
+    <path fill="#84807F" stroke="#fff" stroke-width="2" d="M25 10h13v23H25z" />
+    <path
+      fill="#84807F"
+      stroke="#fff"
+      stroke-width="2"
+      d="m32.22 2.307-.72-.75-.72.75-15.156 15.75-1.63 1.693h35.011l-1.629-1.693-15.155-15.75Z"
+    />
+    <path fill="#84807F" d="M26 16h11v9H26z" />
+  </svg>
+);
 
 export const PullIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="58" height="45" fill="none" viewBox="0 0 58 45">
-  <path fill="#fff" stroke="#84807F" stroke-width="2.25" d="M1.875 26.875h54.25v16.25H1.875z"/>
-  <path stroke="#84807F" stroke-width="2.25" d="M1.875 26.875h13.25v5.25H1.875z"/>
-  <path fill="#84807F" stroke="#fff" stroke-width="2" d="M38 30.82H25V9.974h13z"/>
-  <path fill="#84807F" stroke="#fff" stroke-width="2" d="M38 10.18H25V5.488h13z"/>
-  <path fill="#84807F" stroke="#fff" stroke-width="2" d="M38 5.692H25V1h13zm-7.182 32.039.682.636.682-.636 15.155-14.134 1.857-1.732H13.806l1.857 1.732L30.817 37.73Z"/>
-  <path fill="#84807F" d="M26 20h11v8H26z"/>
-</svg>
-
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="58"
+    height="45"
+    fill="none"
+    viewBox="0 0 58 45"
+  >
+    <path
+      fill="#fff"
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 26.875h54.25v16.25H1.875z"
+    />
+    <path
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 26.875h13.25v5.25H1.875z"
+    />
+    <path
+      fill="#84807F"
+      stroke="#fff"
+      stroke-width="2"
+      d="M38 30.82H25V9.974h13z"
+    />
+    <path
+      fill="#84807F"
+      stroke="#fff"
+      stroke-width="2"
+      d="M38 10.18H25V5.488h13z"
+    />
+    <path
+      fill="#84807F"
+      stroke="#fff"
+      stroke-width="2"
+      d="M38 5.692H25V1h13zm-7.182 32.039.682.636.682-.636 15.155-14.134 1.857-1.732H13.806l1.857 1.732L30.817 37.73Z"
+    />
+    <path fill="#84807F" d="M26 20h11v8H26z" />
+  </svg>
+);
 
 export const ClearIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="58" height="44" fill="none" viewBox="0 0 58 44">
-  <path fill="#fff" stroke="#84807F" stroke-width="2.25" d="M1.875 15.875h54.25v16.25H1.875z"/>
-  <path stroke="#84807F" stroke-width="2.25" d="M1.875 15.875h13.25v5.25H1.875z"/>
-  <path stroke="#fff" stroke-width="7" d="M18 22.5h-7m41 0h-7"/>
-  <path stroke="#fff" stroke-width="7.5" d="m38.645 14.417 8.507-8.464m-22.29 8.464-8.507-8.464m8.507 23.089-8.507 8.463M31.75 33v11m0-44v11m-19 7v13m34.112 6.358-8.507-8.463"/>
-  <path stroke="#84807F" stroke-width="5" d="M18 21.5h-8m43 0h-8m-21.321-8.318-6.442-6.41m22.526 6.41 6.443-6.41M31.5 9V1m8.263 29.228 6.443 6.409M23.68 30.228l-6.443 6.41m14.705-2.228v8"/>
-</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="58"
+    height="44"
+    fill="none"
+    viewBox="0 0 58 44"
+  >
+    <path
+      fill="#fff"
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 15.875h54.25v16.25H1.875z"
+    />
+    <path
+      stroke="#84807F"
+      stroke-width="2.25"
+      d="M1.875 15.875h13.25v5.25H1.875z"
+    />
+    <path stroke="#fff" stroke-width="7" d="M18 22.5h-7m41 0h-7" />
+    <path
+      stroke="#fff"
+      stroke-width="7.5"
+      d="m38.645 14.417 8.507-8.464m-22.29 8.464-8.507-8.464m8.507 23.089-8.507 8.463M31.75 33v11m0-44v11m-19 7v13m34.112 6.358-8.507-8.463"
+    />
+    <path
+      stroke="#84807F"
+      stroke-width="5"
+      d="M18 21.5h-8m43 0h-8m-21.321-8.318-6.442-6.41m22.526 6.41 6.443-6.41M31.5 9V1m8.263 29.228 6.443 6.409M23.68 30.228l-6.443 6.41m14.705-2.228v8"
+    />
+  </svg>
+);
 
-)
+export const PictoLogo = ({ color }: { color: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="176"
+    height="16"
+    fill="none"
+    viewBox="0 0 176 16"
+  >
+    <path
+      fill={color ? color : "#fff"}
+      d="M0 14V0h12v2h2v6h-2v2H4v4H0Zm4-6h6V2H4v6Zm14 6v-2h4V2h-4V0h12v2h-4v10h4v2H18Zm18 0v-2h-2v-2h-2V4h2V2h2V0h8v2h2v2h-4V2h-4v2h-2v6h2v2h4v-2h4v2h-2v2h-8Zm18 0V2h-4V0h12v2h-4v12h-4Zm46 0v-2h-2v-2h-2V4h2V2h2V0h8v2h2v2h-4V2h-4v2h-2v6h2v2h4v-2h4v2h-2v2h-8Zm14 0V0h4v12h8v2h-12Zm30 0V0h4v2h2v2h2v2h2V0h4v14h-4v-4h-2V8h-2V6h-2v8h-4Zm16 0V0h14v2h-10v4h8v2h-8v4h10v2h-14Z"
+    />
+    <rect
+      width="12"
+      height="12"
+      x="65"
+      y="1"
+      stroke={color ? color : "#fff"}
+      stroke-width="2"
+      rx="2"
+    />
+    <path
+      fill={color ? color : "#fff"}
+      d="M68 4h2v2h-2zm0 4h6v2h-6zm4-4h2v2h-2z"
+    />
+    <rect
+      width="12"
+      height="12"
+      x="129"
+      y="1"
+      stroke={color ? color : "#fff"}
+      stroke-width="2"
+      rx="2"
+    />
+    <path
+      fill={color ? color : "#fff"}
+      d="M138 10h-2V8h2zm0-4h-6V4h6zm-4 4h-2V8h2z"
+    />
+  </svg>
+);
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,69 +1,106 @@
-import { useEffect, useRef, useState } from "react"
-import { useSearchParams } from "react-router-dom"
-import styled from "styled-components"
+import { useEffect, useRef, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+import { PictoLogo } from "../icons/CanvasIcons";
 
 export const Home = () => {
+  let usernameRef = useRef<any>();
+  let roomnameRef = useRef<any>();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [roomId, setRoomId] = useState(() => searchParams.get("room"));
 
-    let usernameRef = useRef<any>()
-    let roomnameRef = useRef<any>()
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [roomId, setRoomId] = useState(()=> searchParams.get('room'))
+  const joinRoom = (e: any) => {
+    e.preventDefault();
+    window.location.href =
+      window.location.origin +
+      `/chat?username=${usernameRef.current.value}&roomname=${roomnameRef.current.value}`;
+  };
 
-    const joinRoom = (e:any) => {
-        e.preventDefault()
-        window.location.href = (window.location.origin+`/chat?username=${usernameRef.current.value}&roomname=${roomnameRef.current.value}`);
-    }
-
-    return (
-        <Centered>
-        <JoinFormContainer>
+  return (
+    <Centered>
+      <JoinFormContainer>
         <form onSubmit={joinRoom}>
-            <h1>PictoClone</h1>
-            <h3>Join a room</h3>
-        <label>username</label>
-        <input type="text" ref={usernameRef} name="username" placeholder="Username" required></input>
-        <label>room number</label>
-        <input type="number" ref={roomnameRef} name="roomNumber" placeholder="Room Number" defaultValue={parseInt(roomId as string) || ''} required></input>
-        <button type="submit" name="submit">Join</button>
+          <PictoLogo color="gray" />
+          <p style={{ color: "gray" }}>Join a room</p>
+
+          <PictoInput
+            type="text"
+            ref={usernameRef}
+            name="username"
+            placeholder="Username"
+            required
+          ></PictoInput>
+
+          <PictoInput
+            type="number"
+            ref={roomnameRef}
+            name="roomNumber"
+            placeholder="Room Number"
+            defaultValue={parseInt(roomId as string) || ""}
+            required
+          ></PictoInput>
+          <SqaureButton type="submit" name="submit">
+            Join
+          </SqaureButton>
         </form>
-        </JoinFormContainer>
-        </Centered>
-    )
-}
+      </JoinFormContainer>
+    </Centered>
+  );
+};
+
+const PictoInput = styled.input`
+  width: 200px;
+  border: 3px solid gray;
+  border-radius: 8px;
+  padding: 5px;
+`;
+
+const SqaureButton = styled.button`
+  width: 100px;
+  height: 30px;
+  border: 3px solid gray;
+  background: lightgray;
+  border-radius: 8px;
+  color: gray;
+  font-size: 16px;
+`;
 
 const Centered = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 100px);
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 100px);
+`;
 
 const JoinFormContainer = styled.div`
-border-radius: 10px;
-background:white;
-border: 3px solid gray;
-width: 100%;
-max-width: 650px;
+  border-radius: 10px;
+  background: white;
+  border: 3px solid gray;
+  width: 100%;
+  max-width: 650px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-& h1 {
+  & h1 {
     text-align: center;
-}
+  }
 
-& h3{
+  & h3 {
     text-align: center;
-}
+  }
 
-    & form {
-        padding: 50px;
-        display: flex;
-        flex-direction: column;
+  & form {
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
 
-        & input {
-            margin-bottom: 10px;
-        }
-
-        & button {
-            margin-top: 10px;
-        }
+    & input {
+      margin-bottom: 10px;
     }
-`
+
+    & button {
+      margin-top: 10px;
+    }
+  }
+`;
